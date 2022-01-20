d7e14df21c89b17f27cfe8a2939abdf3b4f807cd
Matty <matty258987@gmail.com>
2022-01-20 15:27:40 +0000

    generic lists

--- src/GenericsExample.java
+++ src/GenericsExample.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericsExample {
     public static void main(String[] args) {
 
@@ -10,10 +13,22 @@ public class GenericsExample {
         shout("John", 77);
         shout(55, "Hey there");
         shout(new Cat(), new Dog());
+
+        List<Integer> intList = new ArrayList<>();
+        intList.add(3);
+        printList(intList);
+
+        List<Cat> catList = new ArrayList<>();
+        catList.add(new Cat());
+        printList(catList);
     }
 
     public static <T, V> void shout(T thingToShout, V otherThingToShout) {
         System.out.println(thingToShout + "!!!!!!");
         System.out.println(otherThingToShout + "!!!!!!");
+    }
+
+    private static void printList(List<?> myList) {
+        System.out.println();
     }
 }
