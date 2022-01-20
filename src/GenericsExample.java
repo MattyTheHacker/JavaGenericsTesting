b71d80b9a02611b037333ba2539a326305ed7ef8
Matty <matty258987@gmail.com>
2022-01-20 15:24:47 +0000

    add

--- src/GenericsExample.java
+++ src/GenericsExample.java
@@ -6,5 +6,14 @@ public class GenericsExample {
 
         Printer<Double> doublePrinter = new Printer<>(39.55);
         doublePrinter.print();
+
+        shout("John", 77);
+        shout(55, "Hey there");
+        shout(new Cat(), new Dog());
+    }
+
+    public static <T, V> void shout(T thingToShout, V otherThingToShout) {
+        System.out.println(thingToShout + "!!!!!!");
+        System.out.println(otherThingToShout + "!!!!!!");
     }
 }
